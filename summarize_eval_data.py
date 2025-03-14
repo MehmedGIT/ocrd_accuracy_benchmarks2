d599f90a86e678e424d2e97cb524752257525403
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2025-03-14 13:23:46 +0100

    add: eval data summary script and results

---
+++ summarize_eval_data.py
@@ -0,0 +1,34 @@
+from json import dumps, load
+from pathlib import Path
+
+script_dir = Path(__file__).resolve().parent
+default_workflow_results_path = Path(f"{script_dir}/results_default_workflow")
+odem_workflow_results_path = Path(f"{script_dir}/results_odem_workflow")
+
+summarized_results = {}
+
+def read_workspace_eval_jsons(ws_path: Path):
+    combined_results = []
+    eval_f_grp = Path(f"{ws_path}/OCR-D-EVAL-SEG-LINE")
+    for curr_file in eval_f_grp.iterdir():
+        if curr_file.is_file() and curr_file.suffix == ".json":
+            with Path(curr_file).open("r", encoding="UTF-8") as f_json:
+                 curr_json = load(f_json)
+                 combined_results.append(curr_json)
+    return sorted(combined_results, key=lambda x: x['gt'])
+
+def fill_summarized_results(path: Path, wf_name: str):
+    summarized_results[wf_name] = {}
+    for curr_file in path.iterdir():  
+        if curr_file.is_dir():
+            ws_json = read_workspace_eval_jsons(curr_file)
+            ws_name = Path(curr_file).name
+            summarized_results[wf_name][ws_name] = ws_json
+    summarized_results[wf_name] = dict(sorted(summarized_results[wf_name].items()))
+
+fill_summarized_results(default_workflow_results_path, "default_workflow.nf")
+fill_summarized_results(odem_workflow_results_path, "odem_workflow.nf")
+
+with open('summarized_results.json', 'w') as results_file: 
+     results_file.write(dumps(summarized_results, indent=4))
+
